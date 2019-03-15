ddf5626cf3c94f7563aee799ea61e323c67b6948
Luiz Mario Lustosa Pascoal <43415130+luizmlpascoal@users.noreply.github.com>
2019-03-14 21:46:27 -0300

    Update App.java

--- ProjetoGIT/src/main/java/br/senaigo/ProjetoGIT/App.java
+++ ProjetoGIT/src/main/java/br/senaigo/ProjetoGIT/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Bem-vindos ao mundo do Git" );
+        System.out.println("Mudança pelo Github.");
     }
 }
