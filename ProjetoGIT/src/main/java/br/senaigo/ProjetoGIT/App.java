0766a95e2724a1b3009c052cb001cc234dc36615
Luiz Mario <Luiz Mario@DESKTOP-BEJ0I1A>
2019-03-14 21:37:35 -0300

    Meu primeiro commit.

---
+++ ProjetoGIT/src/main/java/br/senaigo/ProjetoGIT/App.java
@@ -0,0 +1,13 @@
+package br.senaigo.ProjetoGIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bem-vindos ao mundo do Git" );
+    }
+}
